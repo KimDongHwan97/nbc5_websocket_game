3553af5a7565d282f8a36a6aeb5f116dbfeef9ab
KimDongHwan <kdhn712@gmail.com>
2024-12-20 10:16:20 +0900

    Create stage.model.js

---
+++ nbc5_websocket_game/src/models/stage.model.js
@@ -0,0 +1,25 @@
+// 스테이지 정보를 객체에 {key: uuid, value: array}의 형태로 uuid를 Key로 저장합니다.
+// value:array 에는 stageId를 가진 객체가 들어갑니다.
+const stages = {};
+
+export const createStage = (uuid) => {
+  stages[uuid] = []; // 초기 스테이지 배열 생성
+};
+
+export const getStage = (uuid) => {
+  return stages[uuid];
+};
+
+// 기존에 stageId 만 저장하던 것에서
+// stageId와 timestamp 2가지를 같이 저장
+export const setStage = (uuid, id, timestamp) => {
+  return stages[uuid].push({ id, timestamp, itemScore: 0 }); // itemScore 추가
+};
+
+export const addItemScore = (uuid, id, score) => {
+  stages[uuid].find((stage) => stage.id === id).itemScore += score;
+}
+
+export const clearStage = (uuid) => {
+  return stages[uuid] = [];
+}
